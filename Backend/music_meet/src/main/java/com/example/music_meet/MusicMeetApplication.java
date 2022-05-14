1e138715912ea9ea0afe62c99110ed2754722c67
ycs-202007055 <abc50050@naver.com>
2022-05-15 00:15:10 +0900

    backend 환경설정
    백엔드 환경설정 테스트
    
    가이드 추가

---
+++ Backend/music_meet/src/main/java/com/example/music_meet/MusicMeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMeetApplication.class, args);
+	}
+
+}
