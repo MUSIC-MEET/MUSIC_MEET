cf3ddbd452b2e497836ac454ab1eab4bfd434728
dobidugi <dobidugi@gmail.com>
2022-05-16 21:24:36 +0900

    Menu 열림 및 닫힘 기능 테스트 및 구현 완료

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,25 @@
 import React, { useCallback, useState } from "react";
 import classes from "./App.module.css";
 import Menu from "./components/Menu/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 function App() {
     const [menu, setMenu] = useState(true);
-    const menuCloseHandler = useCallback(() => {
-        setMenu(false);
+    const menuVisibleHandler = useCallback(() => {
+        setMenu((prevState) => !prevState);
     },[]);
     return (
         <div>
             <div className={classes.content}>
                 { menu && <Menu 
                     className={classes.nav}
-                    onMenuClose={menuCloseHandler}
+                    onMenuClose={menuVisibleHandler}
                 />}
-                dd
+                {
+                    !menu && <MenuIcon
+                        className={classes["menu-open"]}
+                        onClick={menuVisibleHandler}
+                    />
+                }
             </div>
         </div>
     );
