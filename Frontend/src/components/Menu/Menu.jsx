15b15d11974c3c98c012a6a96e8a9fc67dc6129b
dobidugi <dobidugi@gmail.com>
2022-05-17 10:44:42 +0900

    css in js emotion으로 변경

--- Frontend/src/components/Menu/Menu.jsx
+++ Frontend/src/components/Menu/Menu.jsx
@@ -1,23 +1,38 @@
+/** @jsxImportSource @emotion/react */
 import React, { useContext } from "react";
 import CloseIcon from "@mui/icons-material/Close";
-import classes from "./Menu.module.css";
+import { css } from "@emotion/react";   
 import styled from "styled-components";
 import ThemeContext from "../../store/ThemeContext";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import NightsStayIcon from "@mui/icons-material/NightsStay";
-const MenuNav = styled.nav`
-    background: ${props => props.background};
-    color: ${props => props.fontColor};
+
+const navStyle = css`
+    width: 15vw;
+    min-width: 15rem;
+    max-width: 60rem;
+    height: 100vh;
+    border-right: 2px solid gainsboro;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center; 
+    & > :nth-child(1) {
+        color: red;
+        align-self: flex-end;
+        margin: 1rem;
+    }
 `;
 function Menu(props) {
     const ctx = useContext(ThemeContext);
     const { theme, setDarkTheme, setLightTheme } = ctx;
     const { background, fontColor } = ctx.themeStyle.menu;
-    const { className, onMenuClose } = props;
+    const { className, onMenuClose } = props;  
     return (
-        <MenuNav background={background} fontColor={fontColor} className={`${className} ${classes.nav}`}>
+        <div 
+            css={[navStyle, css` background: ${background}; color: ${fontColor};`]}
+        >
             <CloseIcon 
-                className={`${classes["menu-close"]} ${classes["nav-item"]}`}
                 onClick={onMenuClose}
             />
             {
@@ -25,8 +40,12 @@ function Menu(props) {
                     <Brightness4Icon onClick={setLightTheme} />:
                     <NightsStayIcon onClick={setDarkTheme} />
             }
-        </MenuNav>
+            <p className={"item"}>test</p>
+        </div>
     );
 }
 
-export default Menu;
+export default styled(Menu)`
+    background-color: ${props => props.background};
+    color: ${props => props.fontColor};
+`;
--- Frontend/src/components/UI/Content.jsx
+++ Frontend/src/components/UI/Content.jsx
@@ -1,22 +1,24 @@
-
+/** @jsxImportSource @emotion/react */
 import React, { useContext } from "react";
-import styled from "styled-components";
+import { css } from "@emotion/react";
 import ThemeContext from "../../store/ThemeContext";
 
-const ContentBlock = styled.div`
+
+const divStyle = css`
     width: 100vw;
     height: 100vh;
-    background: ${ props => props.background };
-    color: ${ props => props.fontColor};
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
 `;
 
 function Content(props) {
     const ctx = useContext(ThemeContext);
     const { background, fontColor } = ctx.themeStyle.content;
     return (
-        <ContentBlock  background={background} fontColor={fontColor}>
+        <div css={[divStyle, css`background: ${background}; color: ${fontColor};`]}>
             {props.children}
-        </ContentBlock>
+        </div>
     );
 }
 
