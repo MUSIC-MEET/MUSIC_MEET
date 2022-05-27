ac27142eafab44291440653d41f568187262548f
kimjungryul <kj605205@nate.com>
2022-05-28 01:54:35 +0900

    로그인 기능 추가

--- Backend/music_meet/src/main/java/com/example/music_meet/Service/AccountService.java
+++ Backend/music_meet/src/main/java/com/example/music_meet/Service/AccountService.java
@@ -4,6 +4,9 @@ package com.example.music_meet.Service;
 import java.sql.*;
 import com.example.music_meet.Model.Account;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 public class AccountService {
     String mysqlurl = "jdbc:mysql://localhost:3306/music_meet?serverTimezone=UTC&characterEncoding=UTF-8";
     String mysqlid = "root";
@@ -17,13 +20,12 @@ public class AccountService {
     //
     // 회원 추가
     //
-    public String createAccountFunc(String ID, String PW, String EMAIL, boolean AGREE1, boolean AGREE2, boolean AGREE3, int STATE)
-    {
+    public String createAccountFunc(String ID, String PW, String EMAIL, boolean AGREE1, boolean AGREE2, boolean AGREE3, int STATE) {
         String sql = "insert into account values(?,?,?,?,?,?,?)";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(mysqlurl,mysqlid,mysqlpassword);
+            conn = DriverManager.getConnection(mysqlurl, mysqlid, mysqlpassword);
             pstmt = conn.prepareStatement(sql);
 
             pstmt.setString(1, ID);
@@ -39,12 +41,12 @@ public class AccountService {
             System.out.println(rsInt);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
                 if (pstmt != null && conn != null)
                     pstmt.close();
                 conn.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -56,8 +58,7 @@ public class AccountService {
     //
     // ID 조회
     //
-    public Account searchAccountFunc(String ID)
-    {
+    public Account searchAccountFunc(String ID) {
         String sql = "select * from account where id=" + ID;
         String id = null;
         String pw = null;
@@ -74,15 +75,14 @@ public class AccountService {
 
             rs = pstmt.executeQuery();
 
-            while (rs.next())
-            {
+            while (rs.next()) {
                 id = rs.getString(1);
                 pw = rs.getString(2);
                 email = rs.getString(3);
-                ag1=  rs.getBoolean(4);
-                ag2=  rs.getBoolean(5);
-                ag3 =  rs.getBoolean(6);
-                state= rs.getInt(7);
+                ag1 = rs.getBoolean(4);
+                ag2 = rs.getBoolean(5);
+                ag3 = rs.getBoolean(6);
+                state = rs.getInt(7);
             }
 
         } catch (Exception e) {
@@ -99,10 +99,50 @@ public class AccountService {
                 e.printStackTrace();
             }
         }
-        Account account = new Account(id,pw,email,ag1,ag2,ag3,state);
+        Account account = new Account(id, pw, email, ag1, ag2, ag3, state);
         return account;
     }
 
+    public String loginFunc(String ID, String PW, HttpServletRequest request) {
+        String sql = "select * from member where id= " + request.getParameter("id") ;
+        String id = request.getParameter("id");
+        String pw = request.getParameter("pw");
+        String idck;
+        String pwck;
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(mysqlurl, mysqlid, mysqlpassword);
+            pstmt = conn.prepareStatement(sql);
 
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                idck = rs.getString(1);
+                pwck = rs.getString(2);
+
+                if (idck.equals(id) && pwck.equals(pw)) {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("id", rs.getString(1));
+                    session.setAttribute("email", rs.getString(3));
+
+                } else {
+                    System.out.println("로그인 실패");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println();
+            e.printStackTrace();
+        } finally {
+
+            try {
+                rs.close();
+                pstmt.close();
+                conn.close();
+            } catch (Exception e) {
+                System.out.println();
+                e.printStackTrace();
+            }
+        } return "";
+    }
 }
