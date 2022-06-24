869d6daffdf895cd8db7689bb70dde3fdb84584c
dobidugi <dobidugi@gmail.com>
2022-06-24 19:24:29 +0900

    package.json homepage 수정

--- Frontend/config-overrides.js
+++ Frontend/config-overrides.js
@@ -4,5 +4,6 @@ const {
     removeModuleScopePlugin,
     override,
 } = require("customize-cra");
-  
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
 module.exports = override(useBabelRc(), removeModuleScopePlugin());
