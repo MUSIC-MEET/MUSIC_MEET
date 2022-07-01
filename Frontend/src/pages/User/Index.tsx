b6ba2921d60bcdce6d7d3df935c9cca8e41deef8
dobidugi <dobidugi@gmail.com>
2022-07-01 22:21:43 +0900

    add: MyPage 라우터 추가

--- Frontend/src/pages/User/Index.tsx
+++ Frontend/src/pages/User/Index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Route, Routes, useLocation, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import MyPage from "./MyPage/MyPage";
 import ResetPassWord from "./ResetPassWord/ResetPassWord";
 function Index() {
     return (
         <div>
             <Routes>
                 <Route path="resetpw/:key" element={<ResetPassWord />} />
+                <Route path="mypage" element={<MyPage />} />
             </Routes>
         </div>
     );
