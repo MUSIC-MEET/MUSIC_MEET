ce52289c226c57876235ebc9b6b8139606b87835
dobidugi <dobidugi@gmail.com>
2022-07-12 15:32:43 +0900

    add: 인증필요한 페이지에서 인증없이 접속시 로그인창띄우기

--- Frontend/src/components/common/PrivateRoute.tsx
+++ Frontend/src/components/common/PrivateRoute.tsx
@@ -1,19 +1,32 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
+import Content from "components/UI/Content";
+import React, { useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import LoginModalShownState from "store/LoginModalShown";
 import LoginState from "store/LoginState";
 
 interface PrivateRoutProps {
     RouteComponent: React.ComponentType;
 }
 
+function Login() {
+    const setShown = useSetRecoilState(LoginModalShownState);
+    useEffect(() => {
+        setShown(true);
+    }, []);
+    return <Content>로그인 필요</Content>;
+}
+
 function PrivateRoute(props: PrivateRoutProps) {
     const { isLogIn } = useRecoilValue<{ isLogIn: boolean }>(LoginState);
     const { RouteComponent } = props;
+    useEffect(() => {
+        //
+    }, [isLogIn]);
     if (isLogIn) {
         return <RouteComponent />;
     }
     else {
-        return <div>로그인필요</div>;
+        return <Login />;
     }
 }
 
