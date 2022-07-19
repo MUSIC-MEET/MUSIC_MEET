ce72e7ca9c37b914062143a3155a0677191c750f
dobidugi <dobidugi@gmail.com>
2022-07-19 22:48:08 +0900

    fix: Login 요청부분 react-query로 변경완료, 이메일 미인증시 알려주는 기능 구현

--- Frontend/src/utils/RequestApis/Login/LoginRequest.ts
+++ Frontend/src/utils/RequestApis/Login/LoginRequest.ts
@@ -1,13 +1,19 @@
 import { AxiosResponse } from "axios";
 import customAxios from "../../customAxios";
 
+
+interface successDataType  {
+    nickname: string;
+    token: string;
+}
+
 interface LoginData {
     values: {
         id: string;
         pw: string;
     }
 }
-const LoginRequest= (data: LoginData): Promise<AxiosResponse> => {
+const LoginRequest= (data: LoginData): Promise<AxiosResponse | successDataType> => {
     const { id, pw } = data.values;
     const axios = customAxios();
     return axios({
@@ -21,3 +27,4 @@ const LoginRequest= (data: LoginData): Promise<AxiosResponse> => {
 };
 
 export default LoginRequest;
+export { successDataType } ;
