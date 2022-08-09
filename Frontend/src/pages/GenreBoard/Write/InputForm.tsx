d30578ca627716608b986f67a400a20ed34b246c
dobidugi <dobidugi@gmail.com>
2022-08-09 20:40:55 +0900

    add: 게시판 글쓰기 페이지 UI 대강완성

--- Frontend/src/pages/GenreBoard/Write/InputForm.tsx
+++ Frontend/src/pages/GenreBoard/Write/InputForm.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import Form from "components/common/Form";
+import Input from "components/common/Input";
 import React from "react";
 
 function InputForm() {
@@ -9,15 +10,42 @@ function InputForm() {
             direction={"column"}
             addCss={[formStyle]}
         >
-            InputForm
+            <span>
+                <label htmlFor="title">제목</label>
+                <Input
+                    w={"100%"}
+                    h={"3rem"}
+                    input={{
+                        placeholder: "제목을 입력하세요",
+                        name: "title",
+                        id: "title",
+                        type: "text"
+                    }}
+                />
+            </span>
+
+            <span>
+                <label htmlFor="content">내용</label>
+            </span>
         </ Form>
     );
 }
 
 const formStyle = css`
-    width: 80rem;
+    margin-top: 3rem;
+    max-width: 100vw;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+
+    span {
+        width: 70rem;
+        margin-bottom: 3rem;
+    }
+
+    span > label {
+        display: block;
+        margin-bottom: 0.5rem;
+    }
 `;
 
 export default InputForm;
