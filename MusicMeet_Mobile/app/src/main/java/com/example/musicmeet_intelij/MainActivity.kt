fa6d58028adbfb08f89490f32e4d5a7fabd6ff11
ycs-201607083 <80020080+ycs-201607083@users.noreply.github.com>
2022-08-11 22:49:38 +0900

    메인.xml ViewPager2를 이용(미완성)

--- MusicMeet_Mobile/app/src/main/java/com/example/musicmeet_intelij/MainActivity.kt
+++ MusicMeet_Mobile/app/src/main/java/com/example/musicmeet_intelij/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.viewpager.widget.ViewPager
 import com.example.musicmeet_intelij.databinding.ActivityMainBinding
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 lateinit var binding: ActivityMainBinding
 
@@ -16,27 +18,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        var ColorCheck = false
-        binding.membersbtn.setOnClickListener{
-            if (!ColorCheck)
-            {
-                binding.membersbtn.isPressed = true
-                binding.membersbtn.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.Black))
-                ColorCheck = false
-            }
-        }
-
-        binding.loginbtn.setOnClickListener{
-            val Login_Intent = Intent(this,Login_Activity::class.java)
+        //로그인 화면 이동
+        binding.loginbtn.setOnClickListener {
+            val Login_Intent = Intent(this, Login_Activity::class.java)
             startActivity(Login_Intent)
         }
-
-        //명시적 intent
-        binding.membersbtn.setOnClickListener{
+        //회원가입 화면 이동
+        binding.membersbtn.setOnClickListener {
             val Members_Intent = Intent(this, Members_Activity::class.java)
             startActivity(Members_Intent)
         }
 
-    }
+        //배경에 나올 이미지
+        var BackgroundImage: Array<Int> = arrayOf(
+            R.drawable.back0, R.drawable.back1, R.drawable.back2,
+            R.drawable.back3, R.drawable.back4, R.drawable.back5,
+            R.drawable.back6, R.drawable.back7, R.drawable.back8,
+            R.drawable.back9, R.drawable.back10, R.drawable.back11
+        )
 
+        var ItemAdapter = ItemAdapter(BackgroundImage)
+
+        binding.viewpager2.apply {
+            adapter = ItemAdapter
+            setPageTransformer(ZoomOutPageTransformer())
+        }
+
+    }
 }
