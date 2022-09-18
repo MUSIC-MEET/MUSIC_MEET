94a877d9099042638fb97932eb829ea33878cf53
dobidugi <dobidugi@gmail.com>
2022-09-18 17:02:31 +0900

    fix: 장르게시판 글 제목으로 검색 수정

--- Frontend/src/pages/GenreBoard/Search/Search.tsx
+++ Frontend/src/pages/GenreBoard/Search/Search.tsx
@@ -1,15 +1,14 @@
 import Title from "components/common/Title";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import style from "../SectionStyle";
 import { useTranslation } from "react-i18next";
 import GenreSelector from "../GenreSelector";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import MoreButton from "../MoreButton";
 import PostList from "../PostList";
 import getSearchList from "utils/RequestApis/GenreBoard/getSearchList";
 import { useMutation } from "react-query";
-import { PostType } from "../PostType";
 
 function Search() {
     const { t } = useTranslation<"genreBoardSearchPage">("genreBoardSearchPage");
@@ -17,10 +16,12 @@ function Search() {
     const type = params.type ?? "title";
     const genre = params.genre ?? "kpop";
     const [keyword, setKeyword] = useState<string>(params.keyword ?? "");
+    const navigator = useNavigate();
     const { data, mutate } = useMutation(getSearchList, {
         useErrorBoundary: false,
         retry: 0
     });
+
     const onChangeKeyWord = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setKeyword(() => e.target.value);
     }, []);
@@ -29,7 +30,14 @@ function Search() {
         e.preventDefault();
         if (keyword === "") return;
         mutate({ type, genre, keyword });
-    }, [genre, keyword, mutate, type]);
+        navigator(`/board/${genre}/search/${type}/${keyword}`);
+    }, [genre, keyword, mutate, navigator, type]);
+
+    useEffect(() => {
+        if (keyword.length !== 0) {
+            mutate({ type, genre, keyword });
+        }
+    }, [mutate, genre, type]);
 
     return (
         <section css={style}>
