c15b07c962ad7007927458cd22c18ff8ecb11030
dobidugi <dobidugi@gmail.com>
2022-09-26 00:53:00 +0900

    fix: 음악 페이지 index.tsx 수정

--- Frontend/src/pages/Music/Index.tsx
+++ Frontend/src/pages/Music/Index.tsx
@@ -26,11 +26,6 @@ function Index() {
             <ErrorBoundary>
                 <Suspense fallback={<Loading />}>
                     <MusicInfo musicNum={musicNum} />
-                </Suspense>
-            </ErrorBoundary>
-
-            <ErrorBoundary>
-                <Suspense fallback={<Loading />}>
                     <Comments musicNum={musicNum} />
                 </Suspense>
             </ErrorBoundary>
