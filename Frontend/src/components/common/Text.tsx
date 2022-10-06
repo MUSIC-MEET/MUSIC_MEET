b103ff3df95c22582dc34b57b403ab198306866d
dobidugi <dobidugi@gmail.com>
2022-10-06 11:31:25 +0900

    add: 음악 평가 페이지 가사 줄바꿈 처리 완료

--- Frontend/src/components/common/Text.tsx
+++ Frontend/src/components/common/Text.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-function Text(props: { children: string; className?: string }) {
+function Text(props: { children?: string; className?: string }) {
     return (
         <ReactMarkdown className={props.className}>{`${props.children}`}</ReactMarkdown>
     );
--- Frontend/src/store/ThemeContext.js
+++ Frontend/src/store/ThemeContext.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const ThemeContext = React.createContext({
     theme: "light",
-  
     themeStyle: {
         menu: { fontColor: "" ,login:{ fontColor: "" }, searchBackground: "" },
         content: { fontColor: "" },
@@ -10,6 +9,7 @@ const ThemeContext = React.createContext({
         input: { borderColor : "" },
         secondFont: { fontColor : "" },
         sectionWrapper: { backgroundColor: "", borderColor: "" },
+        fontStyle1: { color: "" },
         fontStyle2: { color : "" },
     },
     setDarkTheme: () => {
