f71d5b94dbba83f251b8cf42e22935dfcfeb49ce
dobidugi <dobidugi@gmail.com>
2022-10-18 16:34:23 +0900

    fix: 메인 페이지 실시간 차트 다국어 미적용이였던거 처리

--- Frontend/src/locales/en/MainPage.ts
+++ Frontend/src/locales/en/MainPage.ts
@@ -2,6 +2,9 @@ import MainPage from "locales/interface/MainPage";
 
 const text: MainPage =  {
     more: "More +",
+    title: {
+        liveChart: "Live Chart",
+    }
 };
 
 export default text;
--- Frontend/src/locales/kr/MainPage.ts
+++ Frontend/src/locales/kr/MainPage.ts
@@ -2,6 +2,10 @@ import MainPage from "locales/interface/MainPage";
 
 const text: MainPage =  {
     more: "더보기 +",
+
+    title: {
+        liveChart: "실시간 차트",
+    }
 };
 
 export default text;
