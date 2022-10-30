598d79884ca99ba1a4c15d040d082facb667cbb0
dobidugi <dobidugi@gmail.com>
2022-10-30 23:10:24 +0900

    add: 음악 플레이어 대강 설계

--- Frontend/src/pages/MusicPlayer/PlayerController/PlayerController.tsx
+++ Frontend/src/pages/MusicPlayer/PlayerController/PlayerController.tsx
@@ -1,24 +1,41 @@
 import { css } from "@emotion/react";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import MusicProgressbar from "./MusicProgressbar";
 import RightController from "./RightController";
 import PlayMusicInfo from "./PlayMusicInfo";
 import ThemeContext from "store/ThemeContext";
+import { cleanup } from "@testing-library/react";
 
+interface PlayerControllerProps {
+    playingMusic: string;
+    prev: () => void;
+    next: () => void;
+}
 
-
-function PlayerController() {
+function PlayerController(props: PlayerControllerProps) {
+    useEffect(() => {
+        console.log(props.playingMusic);
+        const audio = new Audio(props.playingMusic);
+        audio.play();
+        return () => {
+            audio.pause();
+        };
+    }, [props.playingMusic]);
     const ctx = useContext(ThemeContext);
     return (
         <section css={[style, css`background: ${ctx.themeStyle.musicPlayer.background};`]}>
             <MusicProgressbar />
             <div className="button-controller">
+                <div>
+                    <button className="prev" onClick={() => props.prev()}>이전</button>
+                    <button className="play" onClick={() => props.next()}>다음</button>
+                </div>
                 <div className="item">1</div>
                 <PlayMusicInfo />
                 <RightController />
             </div>
 
-        </section>
+        </section >
     );
 }
 
