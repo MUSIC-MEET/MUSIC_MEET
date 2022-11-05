c629544dbd0cee79ba1a1827de28917cbc342490
AntenA <80020067+ycs-202007055@users.noreply.github.com>
2022-11-05 23:46:37 +0900

    fix: playlist테이블에 playlist 속성에 디폴트 값 ''->'[]'로 변경

--- Backend/music_meet/sql/InitSQL.sql
+++ Backend/music_meet/sql/InitSQL.sql
@@ -1593,7 +1593,7 @@ REFERENCES `User` (
 CREATE TABLE `PlayList` (
 	`playlistnum` INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`usernum` INT(10) NOT NULL,
-	`playlist` VARCHAR(3000) NOT NULL default ''
+	`playlist` VARCHAR(3000) NOT NULL default '[]'
 );
 
 ALTER TABLE `PlayList` ADD CONSTRAINT `FK_User_TO_PlayList_1` FOREIGN KEY (
@@ -1602,4 +1602,3 @@ ALTER TABLE `PlayList` ADD CONSTRAINT `FK_User_TO_PlayList_1` FOREIGN KEY (
 REFERENCES `User` (
 	`usernum`
 );
-
