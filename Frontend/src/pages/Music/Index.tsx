97be2c5aee4d88098a7232773dbacd75f09942f3
dobidugi <dobidugi@gmail.com>
2022-11-07 11:57:22 +0900

    fix(issue): Album List Page에서 새로고침시 메뉴 엉뚱한곳 가르키는거 수정

--- Frontend/src/pages/Music/Index.tsx
+++ Frontend/src/pages/Music/Index.tsx
@@ -1,16 +1,21 @@
 import React, { Suspense, useEffect } from "react";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Loading from "components/common/Loading";
 import ErrorBoundary from "./ErrorBoundary";
 
 import List from "./List/List";
 import View from "./View/View";
+import CurrentPage from "store/CurrentPage";
+import { useSetRecoilState } from "recoil";
 /**
  * 음악 페이지 메인
  * @returns 
  */
 function Index() {
-
+    const setCurrentPage = useSetRecoilState(CurrentPage);
+    useEffect(() => {
+        setCurrentPage(3);
+    }, [setCurrentPage]);
     return (
         <React.Fragment>
 
